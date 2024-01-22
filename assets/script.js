c8f2a68f2a56f8b0964c97a1255c972c6f37fcc7
georgielill <georgielills@gmail.com>
2024-01-22 22:50:27 +0000

    reset repo by readding html starter code and restarted js - adding api key variable, search button variable and search button event listener

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+// api key
+const apiKey = 'f7d34248b47264a3f6da9b533f15ac3a';
+
+// target search
+let searchButton = document.getElementById('search-button');
+
+searchButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  let cityName = document.getElementById("search-input").value.trim();
+  //gets city name inputted by user
+  document.getElementById("search-input").
+  //clears city name now it's been stored in cityName variable
+  value = "";
+
+  createNewButton(cityName);
+  todayForecast(cityName);
+  addToStorage(cityName);
+});
+
+
+// let searchInputEl = document.querySelector('#search-input');
+
+// let currentWeatherDiv = document.querySelector(".current-weather");
+// let daysForecastDiv = document.querySelector(".days-forecast");
+
+
+fetch('https://api.openweathermap.org/data/2.5/weather?lat=51.5072&lon=0.1276&appid=f7d34248b47264a3f6da9b533f15ac3a&units=metric')
+  .then(response => response.json())
+  .then(data => console.log(data))
+
+
+
