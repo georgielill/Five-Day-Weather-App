3f1541aed460295a8d7a6c874b73ec19c3f8b8d4
georgielill <georgielills@gmail.com>
2024-01-29 20:30:33 +0000

    copied in css code

--- assets/script.js
+++ assets/script.js
@@ -8,10 +8,8 @@ searchButton.addEventListener("click", function (e) {
   e.preventDefault();
   let cityName = document.getElementById("search-input").value.trim();
   //gets city name inputted by user
-  document.getElementById("search-input").
+  document.getElementById("search-input"). value = "";
   //clears city name now it's been stored in cityName variable
-  value = "";
-
   createNewButton(cityName);
   todayForecast(cityName);
   addToStorage(cityName);
@@ -20,7 +18,6 @@ searchButton.addEventListener("click", function (e) {
 //current weather function
 function todayForecast(cityName) {
   let queryURL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&weather&limit=5&appid=${apiKey}`
-}
 
 //fetch to get latitude and longitude
 
@@ -28,28 +25,63 @@ fetch(queryURL)
 .then(function (response) {
   return response.json();
 })
-.then(function (data)) {
+.then(function (data) {
   let latitude = data[0].lat;
   let longitude = data[0].lon;
-  let forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=f7d34248b47264a3f6da9b533f15ac3a&units=metric'`
-}
+  let forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=f7d34248b47264a3f6da9b533f15ac3a&units=metric`;
 
+  return fetch(forecastURL);
+})
 // fetch to get 5 day forecast
-
-fetch(forecastURL)
+// fetch(forecastURL)
 .then(function (response) {
   return response.json();
 }) 
-.then(function dayFiveForecast (data) {
+.then(function(data) {
+  displayTodayForecast(data.list[0], cityName);
+  displayFiveDayForecast(data);
+})
+.catch(function(error){
+    console.error(`Error fetching data:`, error);
+});
+
+function displayTodayForecast(todayData, cityName) {
+  const cityDisplay = document.getElementById('today');
+  cityDisplay.className = "card d-flex";
+  cityDisplay.innerHTML = "";
+
+  let cityDisplayHeading= document.createElement("h2");
+  cityDisplayHeading.textContent = `${cityName} (${dayjs().format('DD/MM/YYYY')})`;
+  let weatherIconSrc = todayData.weather[0].icon;
+  let weatherIcon = document.createElement('img');
+  weatherIcon.src = `https://openweathermap.org/img/wn/${weatherIconSrc}.png`;
+  cityDisplayHeading.appendChild(weatherIcon);
+
+  let cityTemp = document.createElement('p');
+  cityTemp.textContent = `Temp: ${parseInt(todayData.main.temp)}°C`;
+  let cityWind = document.createElement('p');
+  cityWind.textContent = `Wind: ${data.list[0].wind.speed}KPH`;
+  let cityHumidity = document.createElement('p');
+  cityHumidity.textContent = `Humidity: ${todayData.main.humidity}%`;
+
+  cityHumidity.appendChild(cityDisplayHeading);
+  cityDisplay.appendChild(cityTemp);
+  cityDisplay.appendChild(cityHumidity);
+}
+
+
+
+function displayFiveDayForecast(data) {
   // Target HTML to dynamically update data
+
   const cityDisplay = document.getElementById('today');
   cityDisplay.className = "card d-flex";
   cityDisplay.innerHTML = '';
   let cityDisplayHeading = document.createElement("h2");
-  cityDisplayHeading.textContent = `${cityName} (${dayjs.format(DD/MM/YYYY)})`;
+  cityDisplayHeading.textContent = `${cityName} (${dayjs().format(DD/MM/YYYY)})`;
   let weatherIconSrc = data.list[0].weather[0].icon;
   let weatherIcon = document.createElement('img');
-  weatherIcon.src = 'https://openweathermap.org/img/wn/${weatherIconSrc}.png'
+  weatherIcon.src = `https://openweathermap.org/img/wn/${weatherIconSrc}.png`
   cityDisplayHeading.appendChild(weatherIcon);
   let cityTemp = document.createElement('p');
   let cityWind = document.createElement('p');
@@ -58,10 +90,17 @@ fetch(forecastURL)
   cityWind.textContent = `Wind: ${data.list[0].wind.speed}KPH`
   cityHumidity.textContent = `Humidity: ${data.list[0].main.humidity}%`;
   cityDisplay.appendChild(cityDisplayHeading, cityTemp, cityWind, cityHumidity);
+  
+  fiveDayForecast(data);
+}
 
-  dayFiveForecast(data);
-})
+}
 
+// Define createNewButton function here
+function createNewButton(cityName) {
+  const button = document.createElement('button');
+  button.textContent = cityName;
+}
 
 // let searchInputEl = document.querySelector('#search-input');
 
@@ -69,9 +108,6 @@ fetch(forecastURL)
 // let daysForecastDiv = document.querySelector(".days-forecast");
 
 
-fetch('https://api.openweathermap.org/data/2.5/weather?lat=51.5072&lon=0.1276&appid=f7d34248b47264a3f6da9b533f15ac3a&units=metric')
-  .then(response => response.json())
-  .then(data => console.log(data))
-
-
-
+// fetch('https://api.openweathermap.org/data/2.5/weather?lat=51.5072&lon=0.1276&appid=f7d34248b47264a3f6da9b533f15ac3a&units=metric')
+//   .then(response => response.json())
+//   .then(data => console.log(data))
